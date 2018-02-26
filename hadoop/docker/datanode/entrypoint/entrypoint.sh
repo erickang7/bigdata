3e51a673ed1e45d3c7f093972771f0244581d5fa
Eric Kang <erickangatms@outlook.com>
2018-02-26 10:03:48 -0800

    .

---
+++ hadoop/docker/datanode/entrypoint/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#hdfs namenode -format
+#hadoop-daemon.sh start namenode
+hadoop-daemon.sh start datanode
+#yarn-daemon.sh start resourcemanager
+yarn-daemon.sh start nodemanager
+tail -f /dev/null
